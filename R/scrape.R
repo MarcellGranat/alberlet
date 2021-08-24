a082e3827cec88740158dff7820e9d0b2adc4cd9
Marcell Granát <64445649+MarcellGranat@users.noreply.github.com>
2021-08-25 00:02:33 +0200

    update scrape.r

--- R/scrape.R
+++ R/scrape.R
@@ -35,8 +35,7 @@ for (type in c("lakas", "haz", "szobat-kinalo-hirdetesek")) {
 
 url <- "https://www.alberlet.hu/kiado_alberlet/budapest-12-kerulet-tusnadi-utca-48m2-2-szoba_851916"
 
-
-room_data <- map(url_total, function(url) {
+room_data <- map(unique(url_total), function(url) {
 page <- SafelyRead(url) 
 page %>% 
   html_table(fill = T) %>% 
